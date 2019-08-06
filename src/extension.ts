eb015b0a1ba4d999f82152ea3a5d64d24616d528
Christian Berger <c.berger18@gmail.com>
2019-08-06 11:43:57 +0200

    Implemented Reformat-Button

--- src/extension.ts
+++ src/extension.ts
@@ -20,6 +20,13 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World Test!');
 	});
 
+	let disposable2 = vscode.commands.registerCommand('extension.DisableFibonaccIt', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Goodbye World Test!');
+	});
+
 	context.subscriptions.push(disposable);
 }
 
